48bfabea2e3f5319de4fd37f1ce7c6d4bee1c190
dylan.clement <dylan.clement@xero.com>
2019-02-21 08:30:16 +1100

    Add send message method.

--- Program.cs
+++ Program.cs
@@ -1,21 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Threading;
+
+using Dapper;
+using Npgsql;
 
 namespace postgres_lock_test
 {   
     public class ThreadData
     {
+        public int numRuns;
+        public int numOrgs;
+        public int maxDelayMS;
     };
 
     // Class to represent the MTD Service endpoint, throws an error when called with the same refresh token twice in a row
-    public class MTDService
+    public class HMRSService
     {
         private static Dictionary<int, string> refreshTokens = new Dictionary<int, string>();
 
-        private static Random random = new Random();
-        public static void RefreshToken(int orgId, string token, int maxDelayMS)
+        // NB! Mimics HMRC refresh Service call
+        // - Throws exception when trying to use a refresh token that's already been used
+        public static void RefreshToken(int orgId, string token)
         {
+            // locking to ensure only one thread can call this at a time
             lock (refreshTokens)
             {
                 if (refreshTokens.ContainsKey(orgId) && refreshTokens[orgId] == token)
@@ -24,16 +33,38 @@ namespace postgres_lock_test
                     throw new Exception("Token already refreshed!");
                 }
 
-                // Add some RNG
-                Thread.Sleep(random.Next(1, maxDelayMS));
                 refreshTokens[orgId] = token;
             }
         }
     }
 
-    public class RefreshRepository
-    {
+    public class MTDService
+    {   
+        private static Random random = new Random();
 
+        public static void SendMessage1(int orgId, int maxDelayMS)
+        {
+            using (var conn = new NpgsqlConnection("User ID=dylan.clement;Host=localhost;Port=5432;Database=postgres;Pooling=true;"))
+            {
+                conn.Open();
+
+                var tran = conn.BeginTransaction();
+
+                // Get refresh token from DB
+                var id = conn.QueryFirst<string>($"SELECT RefreshToken FROM public.TestRefreshTokenLock WHERE orgId = {orgId};");
+
+                // Get new token and sleep for some RNG
+                var newToken  = random.Next().ToString();
+                Thread.Sleep(random.Next(1, maxDelayMS));
+
+                // Update refresh token in DB
+                HMRSService.RefreshToken(orgId, newToken);
+                conn.Execute($"UPDATE public.TestRefreshTokenLock SET RefreshToken = {newToken} WHERE orgId = {orgId};");
+
+                // Commit transaction, release SQL locks
+                tran.Commit();
+            }
+        }
     }
 
     class Program
@@ -42,21 +73,26 @@ namespace postgres_lock_test
         public const int NUM_THREADS = 4;
 
         public const int NUM_ORGS = 10;
-        public int MAX_DELAY_IN_MS = 100;
+        public const int MAX_DELAY_IN_MS = 100;
 
         private static Random random = new Random();
     
-        static void ThreadRun(int numRuns, int numOrgs, int maxDelayMS)
+        static void ThreadRun(object stateInfo)
         {
-            HMRSService.RefreshToken(random.Next(0, numOrgs), random.Next().ToString(), maxDelayMS);
+            var data = (ThreadData)stateInfo;
+            for (var i = 0; i < data.numRuns; i++)
+            {
+                MTDService.SendMessage1(random.Next(0, data.numOrgs), data.maxDelayMS);
+            } 
         }
 
         static void Main(string[] args)
         {
             for (int i = 0; i < NUM_THREADS; i++)
             {
-
+                ThreadPool.QueueUserWorkItem(ThreadRun, new ThreadData { numOrgs = NUM_ORGS, numRuns = NUM_RUNS / NUM_THREADS, maxDelayMS = MAX_DELAY_IN_MS});
             }
+            Console.WriteLine($"Successfully ran for {NUM_RUNS} iterations");       
         }
     }
 }
